ac752a911e2a927bf18865b36d7e6f6425efdc29
Alessandro Mulloni <a.mulloni@gmail.com>
2017-01-12 12:17:33 +0100

    Using new BLE scanning API on API21+ devices.

--- rxandroidble/src/main/java/com/polidea/rxandroidble/internal/operations/RxBleRadioOperationScan.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/internal/operations/RxBleRadioOperationScan.java
@@ -1,6 +1,12 @@
 package com.polidea.rxandroidble.internal.operations;
 
+import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
 import com.polidea.rxandroidble.exceptions.BleScanException;
 import com.polidea.rxandroidble.internal.RxBleInternalScanResult;
@@ -21,24 +27,64 @@ public class RxBleRadioOperationScan extends RxBleRadioOperation<RxBleInternalSc
     private volatile boolean isStopped = false;
 
     private final BluetoothAdapter.LeScanCallback leScanCallback = (device, rssi, scanRecord) -> {
-
         if (!hasDefinedFilter() || hasDefinedFilter() && containsDesiredServiceIds(scanRecord)) {
             onNext(new RxBleInternalScanResult(device, rssi, scanRecord));
         }
     };
 
-    public RxBleRadioOperationScan(UUID[] filterServiceUUIDs, RxBleAdapterWrapper rxBleAdapterWrapper, UUIDUtil uuidUtil) {
+    private final ScanCallback leScanCallbackAPI21;
 
+    public RxBleRadioOperationScan(UUID[] filterServiceUUIDs, RxBleAdapterWrapper rxBleAdapterWrapper, UUIDUtil uuidUtil) {
         this.filterServiceUUIDs = filterServiceUUIDs;
         this.rxBleAdapterWrapper = rxBleAdapterWrapper;
         this.uuidUtil = uuidUtil;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            leScanCallbackAPI21 = new ScanCallback() {
+                @Override
+                public void onBatchScanResults(List<ScanResult> results) {
+                    super.onBatchScanResults(results);
+                    for (ScanResult result : results) {
+                        nextScanResult(result);
+                    }
+                }
+
+                @Override
+                public void onScanFailed(int errorCode) {
+                    super.onScanFailed(errorCode);
+                }
+
+                @Override
+                public void onScanResult(int callbackType, ScanResult result) {
+                    super.onScanResult(callbackType, result);
+                    nextScanResult(result);
+                }
+
+                @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                private void nextScanResult(ScanResult scanResult) {
+                    ScanRecord scanRecord = scanResult.getScanRecord();
+                    if (scanRecord != null && scanRecord.getBytes() != null) {
+                        if (!hasDefinedFilter() || hasDefinedFilter() && containsDesiredServiceIds(scanRecord.getBytes())) {
+                            onNext(new RxBleInternalScanResult(scanResult.getDevice(), scanResult.getRssi(), scanRecord.getBytes()));
+                        }
+                    }
+                }
+            };
+        } else {
+            leScanCallbackAPI21 = null;
+        }
     }
 
     @Override
     protected void protectedRun() {
 
         try {
-            boolean startLeScanStatus = rxBleAdapterWrapper.startLeScan(leScanCallback);
+            boolean startLeScanStatus;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                startLeScanStatus = rxBleAdapterWrapper.startLeScan(leScanCallbackAPI21);
+            } else {
+                startLeScanStatus = rxBleAdapterWrapper.startLeScan(leScanCallback);
+            }
 
             if (!startLeScanStatus) {
                 onError(new BleScanException(BleScanException.BLUETOOTH_CANNOT_START));
@@ -64,7 +110,11 @@ public class RxBleRadioOperationScan extends RxBleRadioOperation<RxBleInternalSc
         isStopped = true;
         if (isStarted) {
             // TODO: [PU] 29.01.2016 https://code.google.com/p/android/issues/detail?id=160503
-            rxBleAdapterWrapper.stopLeScan(leScanCallback);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                rxBleAdapterWrapper.stopLeScan(leScanCallbackAPI21);
+            } else {
+                rxBleAdapterWrapper.stopLeScan(leScanCallback);
+            }
         }
     }
 
--- rxandroidble/src/main/java/com/polidea/rxandroidble/internal/util/RxBleAdapterWrapper.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/internal/util/RxBleAdapterWrapper.java
@@ -1,7 +1,11 @@
 package com.polidea.rxandroidble.internal.util;
 
+import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.os.Build;
 import android.support.annotation.Nullable;
 
 import java.util.Set;
@@ -9,9 +13,16 @@ import java.util.Set;
 public class RxBleAdapterWrapper {
 
     private final BluetoothAdapter bluetoothAdapter;
+    private final BluetoothLeScanner bluetoothLeScanner;
 
     public RxBleAdapterWrapper(@Nullable BluetoothAdapter bluetoothAdapter) {
         this.bluetoothAdapter = bluetoothAdapter;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+        } else {
+            bluetoothLeScanner = null;
+        }
     }
 
     public BluetoothDevice getRemoteDevice(String macAddress) {
@@ -32,6 +43,23 @@ public class RxBleAdapterWrapper {
 
     public void stopLeScan(BluetoothAdapter.LeScanCallback leScanCallback) {
         bluetoothAdapter.stopLeScan(leScanCallback);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public boolean startLeScan(ScanCallback callback) {
+        if (bluetoothLeScanner != null) {
+            bluetoothLeScanner.startScan(callback);
+            return true;
+        }
+
+        return false;
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void stopLeScan(ScanCallback callback) {
+        if (bluetoothLeScanner != null) {
+            bluetoothLeScanner.stopScan(callback);
+        }
     }
 
     public Set<BluetoothDevice> getBondedDevices() {
